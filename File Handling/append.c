83548e0242e4ae7ed01a57acd8d95a651690a856
Harish V <109494406+learnerharishV@users.noreply.github.com>
2022-11-21 08:56:12 +0530

    Add files via upload

---
+++ File Handling/append.c
@@ -0,0 +1,27 @@
+ #include <stdio.h>
+
+int main(void) {
+  
+  FILE *fp;
+  char a[20];
+  
+
+  //File Creating or writing
+  
+  fp=fopen("file.text","a");
+  fputc('c',fp); // storing an character
+  fputs("File writing \n",fp); // storing an String
+    // Writing the user inputs
+  gets(a);
+  fputs(a,fp);
+
+    //fprintf(file pointer,types,variables)
+  char myname[20]="File Writing";
+  int myage=21;
+
+  fprintf(fp,"\n %s \n %d" , myname,myage);
+
+  // File Closing
+  fclose(fp);
+  return 0;
+}
